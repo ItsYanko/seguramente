f018bd503501c53a74a9958d0ef230aab736c49b
ItsYanko <ianperina@gmail.com>
2021-02-18 13:45:48 +0000

    sync changes

---
+++ api/sessions.js
@@ -0,0 +1,116 @@
+const storage = require('node-persist');
+const crypto = require('crypto');
+const ms = require("ms");
+const questions = require("../questions.json");
+
+/* Configurações */
+let timeout = ms("15m");
+
+/* Variáveis */
+let keycache = []; // Guarda os NONCES existentes
+let deletecache = {}; // Guarda os IDs dos timeouts das sessões || ESTRUTURA {nonce: id, nonce2: id2, ...}
+
+module.exports = {
+    create: async (name) => {
+        let now = Date.now();
+        let nonce = false;
+        do {
+            let _nonce = crypto.randomBytes(16).toString('hex')
+            if (!keycache.includes(_nonce))
+                nonce = _nonce;
+        } while (!nonce)
+
+        let shuffled = shuffleArray(questions);
+
+        await storage.setItem(`session-${nonce}`, {
+            name: name || "Sem Nome",
+            nonce: nonce,
+            created: now,
+            order: shuffled.map(q => q.index),
+            current: 0,
+            results: {},
+            finished: false
+        })
+        keycache.push(nonce);
+
+        setDeleteTimeout(nonce, now + timeout);
+
+        return { error: false, nonce: nonce }
+    },
+    get: async (nonce) => {
+        if (!keycache.includes(nonce))
+            return { error: true, msg: "Sessão inválida. Experimente atualizar a página" };
+
+        let res = await storage.getItem(`session-${nonce}`);
+
+        if (res.nonce != nonce)
+            return { error: true, msg: "Sessão inválida. Experimente atualizar a página" };
+
+        return { error: false, data: res }
+    },
+    update: async (nonce, newData) => {
+        if (!keycache.includes(nonce))
+            return { error: true, msg: "Sessão inválida. Experimente atualizar a página" };
+
+        await storage.updateItem(`session-${nonce}`, newData);
+
+        return { error: false }
+    },
+    remove: async (nonce) => {
+        let cacheIndex = keycache.indexOf(nonce);
+        if (cacheIndex >= 0)
+            keycache.splice(cacheIndex, 1);
+
+        await storage.removeItem(`session-${nonce}`);
+        return { error: false }
+    },
+    /* Carregar da storage */
+    load: async () => {
+        await storage.init();
+        await storage.forEach(e => {
+            if (e.key.startsWith("session-")) {
+                if (!e.value.finished) {
+                    keycache.push(e.value.nonce);
+                    setDeleteTimeout(e.value.nonce, e.value.created + timeout);
+                }
+            }
+        })
+        return true;
+    }
+}
+
+/* Funções Extra */
+function shuffleArray(array) {
+    let _new = JSON.parse(JSON.stringify(array)); // Unlink
+    for (var i = _new.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = _new[i];
+        _new[i] = _new[j];
+        _new[j] = temp;
+    }
+    return _new;
+}
+
+function setDeleteTimeout(nonce, endtime) {
+    let time = endtime - Date.now();
+    if (time <= 0)
+        time = 1;
+
+    deletecache[nonce] = setTimeout(() => {
+        (async function () {
+            let _nc = nonce;
+            delete deletecache[_nc];
+
+            const info = await module.exports.get(_nc);
+            let del = false;
+            if (info.error || !info.data) {
+                del = true
+            } else if (!info.data.finished) {
+                del = true;
+            }
+
+            if (del)
+                await module.exports.remove(_nc);
+        })();
+    }, time);
+}
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const crypto = require('crypto');
+const cookieParser = require('cookie-parser')
 let app = express();
 
-app.use(express.static("public"))
+/* APIs internos */
+const sessions = require("./api/sessions");
+const questions = require("./api/questions");
+const { json } = require("body-parser");
+
+/* Ficheiros Estáticos */
+app.use(cookieParser());
+app.use(express.static("public"));
 
 /* API */
-app.get("/api/*", (req, res) => {
+app.get("/api/*", async (req, res) => {
     const action = req.path.replace("/api/", "");
 
     switch (action) {
@@ -14,14 +21,81 @@ app.get("/api/*", (req, res) => {
             break;
         }
 
-        case "session/check": {
+        case "session/get": {
+            let _res = await sessions.get(req.cookies.nonce);
+            if (_res.error) {
+                res.cookie('nonce', '');
+                res.json({ error: true, msg: _res.msg || "Sessão inválida", valid: false });
+            } else {
+                const sendData = {
+                    nonce: _res.data,
+                    name: _res.name,
+                    created: _res.created
+                }
+                res.json({ error: false, data: _res.data, valid: true });
+            }
             break;
         }
 
+        case "session/create": {
+            let _res = await sessions.create(req.query.name);
+            if (_res.error) {
+                res.json({ error: true, msg: _res.msg || "Erro ao inicializar o jogo. Tente limpar os cookies." });
+            } else {
+                res.cookie('nonce', _res.nonce);
+                res.json({ error: false, nonce: _res.nonce });
+            }
+            break;
+        }
+
+        case "question/get": {
+            let _res = await questions.getCurrent(req.cookies.nonce);
+            if (_res.error) {
+                res.json({ error: true, msg: _res.msg || "Erro ao obter pergunta" });
+            } else {
+                res.json({ error: false, data: _res.data });
+            }
+            break;
+        }
+
+        case "question/submit": {
+            let _res = await questions.submitAnswer(req.cookies.nonce, req.query.option);
+            if (_res.error) {
+                res.json({ error: true, msg: _res.msg || "Erro a submeter resposta" });
+            } else {
+                res.json(_res);
+            }
+            break;
+        }
+
+        case "question/info/global": {
+            let _res = await questions.info.global();
+            if (_res.error) {
+                res.json({ error: true, msg: _res.msg || "Erro ao obter informação." })
+            } else {
+                res.json({ error: false, data: _res.data });
+            }
+            break;
+        }
+
+        case "question/info/nonce": {
+            let _res = await questions.info.nonce(req.cookies.nonce);
+            if (_res.error) {
+                res.json({ error: true, msg: _res.msg || "Erro ao obter informação." })
+            } else {
+                res.json({ error: false, data: _res.data });
+            }
+            break;
+        }
         default:
             res.json({ error: true, msg: "Endpoint Inválido" });
             break;
     }
-})
+});
 
-app.listen(3005);
+/* Inicialização */
+(async () => {
+    await sessions.load();
+    await questions.load(sessions);
+    app.listen(3005);
+})();
